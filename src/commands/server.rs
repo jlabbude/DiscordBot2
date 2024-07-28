c72a97ee0d2f89c7637b01ea536d1af4d76fd560
Lucas <jlabbude@hotmail.com>
2024-07-28 15:41:53 -0300

    added packet sniffing on port to know amount of players online

--- src/commands/server.rs
+++ src/commands/server.rs
@@ -1,5 +1,8 @@
-use std::net::TcpListener;
 use serenity::all::{CreateCommand, ResolvedOption};
+use std::collections::HashSet;
+use std::net::{Ipv4Addr, TcpStream};
+use std::time::{Duration, SystemTime};
+use pcap::{Capture, Device};
 
 fn get_ip() -> Option<String> {
     get_if_addrs::get_if_addrs()
@@ -12,19 +15,55 @@ fn get_ip() -> Option<String> {
         .cloned()
 }
 
-fn is_port_open(port: u16) -> std::io::Result<TcpListener> {
+fn is_port_open(port: u16) -> std::io::Result<TcpStream> {
     let address = format!("{}:{}", get_ip().unwrap(), port);
-    TcpListener::bind(&address)
+    TcpStream::connect(address)
+}
+
+fn get_ips(target_port: u16) -> Result<HashSet<Ipv4Addr>, String> {
+    let mut unique_ips: HashSet<Ipv4Addr> = HashSet::new();
+    let device = Device::lookup().unwrap().unwrap();
+    let mut capture = Capture::from_device(device)
+        .unwrap()
+        .promisc(true)
+        .snaplen(5000)
+        .timeout(500)
+        .open();
+
+    match capture {
+        Ok(mut cap) => {
+            cap.filter(&format!("tcp dst port {}", target_port), true)
+                .unwrap();
+            let now = SystemTime::now();
+            println!("Listening...");
+            while now.elapsed().unwrap() < Duration::from_millis(800) {
+                if let Ok(packet) = cap.next_packet() {
+                    let ip_header = &packet[14..34];
+                    let src_ip =
+                        Ipv4Addr::new(ip_header[12], ip_header[13], ip_header[14], ip_header[15]);
+                    unique_ips.insert(src_ip);
+                }
+            }
+            println!("Done.");
+            Ok(unique_ips)
+        }
+        Err(_) => Err(String::from("Run with sudo.")),
+    }
 }
 
 pub fn run(_options: &[ResolvedOption]) -> String {
-    String::from(match is_port_open(25565) {
-        // Yes this is correct
-        Err(_) => "O servidor est\u{00E1} **aberto**.",
-        Ok(_) => "O servidor est\u{00E1} **fechado**.",
-    })
+    const PORT: u16 = 25565;
+    match is_port_open(PORT) {
+        Ok(_) => match get_ips(PORT) {
+            Ok(ips) => format!("O servidor est\u{00E1} **aberto** com {:?} jogadores.", ips.len()),
+            Err(e) => format!("O servidor est\u{00E1} **aberto**. {}", e),
+        },
+        Err(_) => String::from("O servidor est\u{00E1} **fechado**."),
+    }
+
+
 }
 
 pub fn register() -> CreateCommand {
-    CreateCommand::new("servidor").description("A ping command")
+    CreateCommand::new("servidor").description("Checar status do servidor")
 }
