6bdcae00d45bfb5b4efd8ddc2a6b791ccc69bf01
Lucas <jlabbude@hotmail.com>
2024-07-28 16:54:27 -0300

    reduced packet ttl and made capture non blocking

--- src/commands/server.rs
+++ src/commands/server.rs
@@ -1,8 +1,8 @@
+use pcap::{Capture, Device};
 use serenity::all::{CreateCommand, ResolvedOption};
 use std::collections::HashSet;
 use std::net::{Ipv4Addr, TcpStream};
 use std::time::{Duration, SystemTime};
-use pcap::{Capture, Device};
 
 fn get_ip() -> Option<String> {
     get_if_addrs::get_if_addrs()
@@ -23,12 +23,14 @@ fn is_port_open(port: u16) -> std::io::Result<TcpStream> {
 fn get_ips(target_port: u16) -> Result<HashSet<Ipv4Addr>, String> {
     let mut unique_ips: HashSet<Ipv4Addr> = HashSet::new();
     let device = Device::lookup().unwrap().unwrap();
-    let mut capture = Capture::from_device(device)
+    let capture = Capture::from_device(device)
         .unwrap()
         .promisc(true)
         .snaplen(5000)
-        .timeout(500)
-        .open();
+        .timeout(100)
+        .open()
+        .unwrap()
+        .setnonblock();
 
     match capture {
         Ok(mut cap) => {
@@ -38,6 +40,7 @@ fn get_ips(target_port: u16) -> Result<HashSet<Ipv4Addr>, String> {
             println!("Listening...");
             while now.elapsed().unwrap() < Duration::from_millis(800) {
                 if let Ok(packet) = cap.next_packet() {
+                    println!("Packet: {:?}", packet);
                     let ip_header = &packet[14..34];
                     let src_ip =
                         Ipv4Addr::new(ip_header[12], ip_header[13], ip_header[14], ip_header[15]);
@@ -47,7 +50,7 @@ fn get_ips(target_port: u16) -> Result<HashSet<Ipv4Addr>, String> {
             println!("Done.");
             Ok(unique_ips)
         }
-        Err(_) => Err(String::from("Run with sudo.")),
+        Err(e) => Err(e.to_string()),
     }
 }
 
@@ -55,13 +58,14 @@ pub fn run(_options: &[ResolvedOption]) -> String {
     const PORT: u16 = 25565;
     match is_port_open(PORT) {
         Ok(_) => match get_ips(PORT) {
-            Ok(ips) => format!("O servidor est\u{00E1} **aberto** com {:?} jogadores.", ips.len()),
+            Ok(ips) => format!(
+                "O servidor est\u{00E1} **aberto** com {:?} jogadores.",
+                ips.len()
+            ),
             Err(e) => format!("O servidor est\u{00E1} **aberto**. {}", e),
         },
         Err(_) => String::from("O servidor est\u{00E1} **fechado**."),
     }
-
-
 }
 
 pub fn register() -> CreateCommand {
